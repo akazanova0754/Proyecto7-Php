70a691483830c275adab1b56a36d9764099ce0ac
marvin0754 <rhyoarias@gmail.com>
2023-08-28 22:24:07 -0500

    Mi aplicacion con PHP v1.1

--- src/config/config.php
+++ src/config/config.php
@@ -1,4 +1,4 @@
 <?php 
 define("URLLOCAL",'http://localhost/Curriculum/Proyecto7-Php/');
-define("URL",'https://ihcproject1.herokuapp.com/');
+define("URL",'web-production-f760.up.railway.app');
 ?>
