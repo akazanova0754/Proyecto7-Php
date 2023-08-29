826e5d6017540b4826f18711c81ee21bf335b88f
marvin0754 <rhyoarias@gmail.com>
2023-08-28 22:35:48 -0500

    Mi aplicacion con PHP v1.3

--- src/config/config.php
+++ src/config/config.php
@@ -1,4 +1,4 @@
 <?php 
 define("URLLOCAL",'http://localhost/Curriculum/Proyecto7-Php/');
-define("URL",'web-production-f760.up.railway.app');
+define("URL",'https://akazanova.000webhostapp.com/');
 ?>
